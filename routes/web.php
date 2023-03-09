0e2ed86ac61d1ccb25e7b05a5a4def28c8181df1
ace34TT <tafinasoa35@gmail.com>
2023-03-10 00:15:15 +0300

    Revert "images"
    This reverts commit 2626b2e481bb8b30fd38805ec478563145761b08.

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//Admin
-Route::group([
-    'prefix' => '/admin',
-    'as' => 'admin.',
-], function () {
-    Route::view("/login", "pages/admin/login");
-    Route::get('/', [AdminController::class, 'index'])->name('index');
-});
