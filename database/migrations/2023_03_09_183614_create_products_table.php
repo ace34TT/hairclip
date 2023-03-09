986a7da848e339b64e7b17d2f2e38d5a3c316b53
ace34TT <tafinasoa35@gmail.com>
2023-03-09 21:49:55 +0300

    migration

--- database/migrations/2023_03_09_183614_create_products_table.php
+++ database/migrations/2023_03_09_183614_create_products_table.php
@@ -11,7 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('value');
+            $table->string("description");
+            $table->decimal("price", 8, 2)->default(7);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('products');
     }
 };
