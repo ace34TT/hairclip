1a3622344bd01e6c96e0478fd57d8d51f6f1db0b
ace34TT <tafinasoa35@gmail.com>
2023-03-11 02:44:53 +0300

    cart

--- resources/views/pages/frontoffice/shopping-cart.blade.php
+++ resources/views/pages/frontoffice/shopping-cart.blade.php
@@ -13,7 +13,6 @@
                         <table class="min-w-full divide-y divide-gray-300">
                             <thead>
                                 <tr>
-
                                     <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold text-gray-900">
                                         Produit</th>
                                     <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold text-gray-900">
@@ -27,11 +26,45 @@
                             <tbody class="divide-y divide-gray-200">
                                 <tr>
                                     <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                                        Lindsay Walton</td>
-                                    <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">Front-end Developer</td>
-                                    <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">lindsay.walton@example.com
+                                        <div class="flex gap-10 items-center">
+                                            <div class="w-56">
+                                                <img class="h-full w-full object-contain"
+                                                    src="{{ asset('images/scranchies/HairClip PackS HD-20.jpg') }}"
+                                                    alt="">
+                                            </div>
+                                            <div>
+                                                <p class="text-2xl font-bold">Hair clip noir</p>
+                                                <p class="underline text-cyan-900">Supprimer</p>
+                                            </div>
+                                        </div>
                                     </td>
-                                    <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">Member</td>
+                                    <td class="whitespace-nowrap py-4 px-3 text-lg font-bold text-black align-middle">
+                                        7€
+                                    </td>
+                                    <td class="whitespace-nowrap py-4 px-3 text-lg align-middle">
+                                        <div class="flex gap-8">
+                                            <span class=" cursor-pointer">-</span>
+                                            <span>1</span>
+                                            <span class="cursor-pointer">+</span>
+                                        </div>
+                                    </td>
+                                    <td class="whitespace-nowrap py-4 px-3 text-sml align-middle">Member</td>
+                                </tr>
+                                {{-- bottom --}}
+                                <tr>
+                                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+
+                                    </td>
+                                    <td class="whitespace-nowrap py-4 px-3 text-lg font-bold text-black align-middle">
+                                        <span>
+                                            Sous-total
+                                        </span>
+                                        <span>7€</span>
+                                    </td>
+                                    <td class="whitespace-nowrap py-4 px-3 text-lg align-middle">
+
+                                    </td>
+                                    <td class="whitespace-nowrap py-4 px-3 text-sml align-middle">Member</td>
                                 </tr>
                                 <!-- More people... -->
                             </tbody>
