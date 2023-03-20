3371e7f4f1d6611e0b67a591d2341def3e14fc52
ace34TT <tafinasoa35@gmail.com>
2023-03-20 08:59:53 +0300

    responsive

--- resources/views/pages/frontoffice/shopping-cart.blade.php
+++ resources/views/pages/frontoffice/shopping-cart.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <x-loading-screen />
     <div
-        class="h-fit min-h-screen flex flex-col @if (Cart::count() === 0) justify-center @else justify-start @endif items-center prose max-w-none">
+        class="h-fit sm:min-h-screen mb-5 flex flex-col gap-0 @if (Cart::count() === 0) justify-center @else justify-start @endif items-center prose max-w-none">
         <h2>Mon panier</h2>
         <a class="text-cyan-800" href="{{ route('homepage') }}">Retrour sur la page d'accueil</a>
         <br>
@@ -19,125 +19,117 @@
                 <input id="rows-json-data" style="border: solid black 1px" type="text" name="updated_data">
                 <input type="submit" id="proceed-to-payment">
             </form>
-            <div class="px-4 w-10/12 overflow-x-hidden sm:px-6 lg:px-8">
-                <div class="mt-8 flow-root">
-                    <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                            <table class="my-0 md:my-8 min-w-full divide-y divide-gray-300">
-                                <thead>
-                                    <tr>
-                                        <th scope="col"
-                                            class="py-3.5 px-3 text-left text-sm font-semibold text-gray-900">
-                                            Produit(s)</th>
-                                        <th scope="col" style="alig"
-                                            class=" py-3.5 px-3 righ text-left text-sm font-semibold text-gray-900">
-                                            Prix</th>
-                                        <th scope="col"
-                                            class=" py-3.5 px-3 text-center text-sm font-semibold text-gray-900">
-                                            Quantité</th>
-                                        <th scope="col"
-                                            class="only py-3.5 px-3 text-left text-sm font-semibold text-gray-900">
-                                            Total</th>
-                                    </tr>
-                                </thead>
-                                <tbody class="divide-y divide-gray-200">
-                                    {{-- main data --}}
-                                    @foreach (Cart::content() as $cart_item)
-                                        <tr class="cart-item-row" data-quantity="{{ $cart_item->qty }}"
-                                            data-row-id="{{ $cart_item->rowId }}">
-                                            <td
-                                                class="whitespace-nowrap pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                                                <div class="flex gap-10 items-center">
-                                                    <div class="w-fit md:w-56">
-                                                        <img class="h-28 w-28 object-contain my-0"
-                                                            src="{{ asset('images/scranchies/' . $cart_item->options['top_view']) }}"
-                                                            alt="">
-                                                    </div>
-                                                    <div>
-                                                        <p class="text-2xl mb-0 mt-0 md:mt-8 font-bold">
-                                                            {{ $cart_item->model->name }}
-                                                            <span data-id="{{ $cart_item->id }}"
-                                                                class="product_id hidden"></span>
-                                                        </p>
-                                                        <a class="underline text-cyan-900"
-                                                            href="{{ route('shopping-cart.remove-item', ['rowId' => $cart_item->rowId]) }}">Supprimer</a>
-                                                    </div>
+            <div class="px-4sm:px-6 lg:px-8 w-11/12 md:w-10/12 ">
+                <div class="-mx-4 flow-root sm:mx-0">
+                    <table class="min-w-full divide-y divide-gray-300">
+                        <thead>
+                            <tr>
+                                <th scope="col"
+                                    class="py-4 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                    Produit(s)
+                                </th>
+                                <th scope="col"
+                                    class="hidden py-4 px-3 text-right text-sm font-semibold text-gray-900 sm:table-cell">
+                                    Prix</th>
+                                <th scope="col" class="py-4 px-3 text-right text-sm font-semibold text-gray-900 sm:pr-0">
+                                    Quantité
+                                </th>
+                                <th scope="col"
+                                    class="hidden py-4 pl-3 pr-4 text-right text-sm font-semibold text-gray-900 sm:pr-0 sm:table-cell">
+                                    Total
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach (Cart::content() as $cart_item)
+                                <tr class="border-b border-gray-200">
+                                    <td class="py-4 pl-4 pr-3 text-sm sm:pl-0">
+                                        <div class="font-medium text-gray-900">
+                                            <div class=" flex gap-10 items-center">
+                                                <div class="sr-only md:not-sr-only w-fit md:w-fit">
+                                                    <img class="h-28 w-28 mr-0 object-contain my-0"
+                                                        src="{{ asset('images/scranchies/' . $cart_item->options['top_view']) }}"
+                                                        alt="">
                                                 </div>
-                                            </td>
-                                            <td class="whitespace-nowrap  px-3 text-lg font-bold text-black align-middle">
-                                                <span class="text-center" data-price=""
-                                                    id="{{ 'product_' . $cart_item->id . '_price' }}">
-                                                    {{ $cart_item->price }}</span>
-                                                €
-                                            </td>
-                                            <td class="whitespace-nowrap  px-3 text-lg align-middle">
-                                                <div class="flex justify-center gap-4">
-                                                    <x-ei-minus
-                                                        class="cursor-pointer w-7 h-7 flex justify-center items-center text-3xl text-black "
-                                                        onclick="updateQuantity('{{ $cart_item->rowId }}', {{ $cart_item->id }} , -1)" />
-                                                    <span data-quantity=""
-                                                        id={{ 'product_' . $cart_item->id . '_quantity' }}>{{ $cart_item->qty }}
-                                                    </span>
-                                                    <x-ei-plus class="cursor-pointer w-7 h-7  text-3xl text-black"
-                                                        onclick="updateQuantity('{{ $cart_item->rowId }}', {{ $cart_item->id }} , 1)" />
+                                                <div>
+                                                    <p class="text-2xl mb-0 mt-0 font-bold">
+                                                        {{ $cart_item->model->name }}
+                                                        <span data-id="{{ $cart_item->id }}"
+                                                            class="product_id hidden"></span>
+                                                    </p>
+                                                    <a class="underline mt-3 text-cyan-900"
+                                                        href="{{ route('shopping-cart.remove-item', ['rowId' => $cart_item->rowId]) }}">Supprimer</a>
                                                 </div>
-                                            </td>
-                                            <td class="whitespace-nowrap  px-3 text-lg font-bold text-black align-middle">
-                                                <span data-subtotal="" id="{{ 'product_' . $cart_item->id . '_total' }}"
-                                                    class="sub_total_price">
-                                                    {{ $cart_item->price * $cart_item->qty }}</span> €
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                    {{-- bottom --}}
-                                    <tr>
-                                        <td class="whitespace-nowrap  pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                                        </td>
-                                        <td class="whitespace-nowrap  px-3 text-lg font-bold text-black align-middle">
-                                        </td>
-                                        <td class="whitespace-nowrap  px-3 text-lg align-middle">
-                                        </td>
-                                        <td class="whitespace-nowrap  px-3 text-sml align-middle text-xl">
-                                            <span class="text-black font-bold">
-                                                Sous-total :
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td
+                                        class="hidden align-middle py-4 px-3 text-right text-sm text-gray-500 sm:table-cell">
+                                        <span class="text-center" data-price=""
+                                            id="{{ 'product_' . $cart_item->id . '_price' }}">
+                                            {{ $cart_item->price }}</span>
+                                        €
+                                    </td>
+                                    <td class="align-middle py-4 px-3 text-right text-sm text-gray-500 ">
+                                        <div class="flex justify-end items-center gap-4">
+                                            <x-ei-minus
+                                                class="cursor-pointer w-7 h-7 flex justify-center items-center text-3xl text-black "
+                                                onclick="updateQuantity('{{ $cart_item->rowId }}', {{ $cart_item->id }} , -1)" />
+                                            <span data-quantity=""
+                                                id={{ 'product_' . $cart_item->id . '_quantity' }}>{{ $cart_item->qty }}
                                             </span>
-                                            <span data-total="" class="text-black font-bold underline">
-                                                <span id="total_price"></span>
-                                                €</span>
-                                        </td>
-                                    </tr>
-                                    {{--  --}}
-                                    <tr>
-                                        <td class="whitespace-nowrap  pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                                            <h3>Livraison : </h3>
-                                            <ul class="ml-3">
-                                                <li>1.99 € pour un achat de moins de 3 chouchou</li>
-                                                <li>4.99 € pour un achat de plus de 3 chouchou</li>
-                                                <li>Expédition en 24h et livraison sous 48 / 72h</li>
-                                            </ul>
-                                        </td>
-                                        <td class="whitespace-nowrap  px-3 text-lg font-bold text-black align-middle">
-                                        </td>
-                                        <td class="whitespace-nowrap  px-3 text-lg align-middle">
-                                        </td>
-                                        <td class="whitespace-nowrap  px-3 text-sml align-middle">
-                                            <button onclick="proceedToPayment()" type="button"
-                                                class="rounded-md bg-cyan-800 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                                onclick="postData()">
-                                                <span id="is-not-loading"> Paaser la commande</span>
-                                                </span>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                    <!-- More people... -->
-                                </tbody>
-                            </table>
-                        </div>
+                                            <x-ei-plus class="cursor-pointer w-7 h-7  text-3xl text-black"
+                                                onclick="updateQuantity('{{ $cart_item->rowId }}', {{ $cart_item->id }} , 1)" />
+                                        </div>
+                                    </td>
+                                    <td
+                                        class="hidden sm:table-cell py-4 pl-3 pr-4 align-middle text-right text-sm text-gray-500 sm:pr-0">
+                                        <span data-subtotal="" id="{{ 'product_' . $cart_item->id . '_total' }}"
+                                            class="sub_total_price">
+                                            {{ $cart_item->price * $cart_item->qty }}</span> €
+                                    </td>
+                                </tr>
+                            @endforeach
+                            <!-- More projects... -->
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th scope="row" colspan="3"
+                                    class="hidden pl-4 pr-3 pt-4 pb-2 text-right text-sm font-semibold text-gray-900 sm:table-cell sm:pl-0">
+                                    Total</th>
+                                <th scope="row"
+                                    class="pl-6 pr-3 pt-4 pb-2 text-left text-sm font-semibold text-gray-900 sm:hidden">
+                                    Total
+                                </th>
+                                <td class="pl-3 pr-4 pt-4 text-right text-sm font-semibold text-gray-900 sm:pr-0"> <span
+                                        id="total_price"></span>
+                                    €</span>
+                                </td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+                <div class="flex flex-col sm:flex-row justify-between items-center sm:items-start mx-4">
+                    <div class="leading-4">
+                        <h3 class="mt-0">Livraison : </h3>
+                        <ul class="ml-3">
+                            <li>1.99 € pour un achat de moins de 3 chouchou</li>
+                            <li>4.99 € pour un achat de plus de 3 chouchou</li>
+                            <li>Expédition en 24h et livraison sous 48 / 72h</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <button onclick="proceedToPayment()" type="button"
+                            class="rounded-md bg-cyan-800 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            onclick="postData()">
+                            <span id="is-not-loading"> Paaser la commande</span>
+                            </span>
+                        </button>
                     </div>
                 </div>
+
             </div>
         @endif
-        {{-- hidden form that will contains all the data to update or to proceed form payment --}}
     </div>
 @endsection
 @section('script')
